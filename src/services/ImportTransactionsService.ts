801c898d8e2a86433c3187e786b7cd39e124aca7
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-24 17:30:57 -0300

    Mudança import

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -6,11 +6,18 @@ import Transaction from '../models/Transaction';
 import CreateTransactionService from './CreateTransactionService';
 import uploadConfig from '../config/multer';
 
+interface CSVTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class ImportTransactionsService {
   async execute(filename: string): Promise<Transaction[]> {
     const createTransactionService = new CreateTransactionService();
 
-    const csvFilePath = path.resolve(uploadConfig.directory, filename);
+    const csvFilePath = path.join(uploadConfig.directory, filename);
 
     const readCSVStream = fs.createReadStream(csvFilePath);
 
@@ -23,28 +30,31 @@ class ImportTransactionsService {
     const parseCSV = readCSVStream.pipe(parseStream);
 
     const transactions: Transaction[] = [];
-    const lines: [] = [];
+    const lines: CSVTransaction[] = [];
 
     parseCSV.on('data', async line => {
-      lines.push(line);
+      const [title, type, value, category] = line.map((cell: string) => cell);
+
+      if (!title || !type || !value || !category) return;
+      lines.push({ title, type, value, category });
     });
 
     await new Promise(resolve => {
-      parseCSV.on('end', async () => {
-        const promises = await lines.map(async line => {
-          const transaction = await createTransactionService.execute({
-            title: line[0],
-            type: line[1],
-            value: line[2],
-            category_title: line[3],
-          });
-
-          transactions.push(transaction);
-        });
-
-        return Promise.all(promises).then(() => resolve());
-      });
+      parseCSV.on('end', resolve);
     });
+
+    const promises = await lines.map(async line => {
+      const transaction = await createTransactionService.execute({
+        title: line.title,
+        type: line.type,
+        value: line.value,
+        category_title: line.category,
+      });
+
+      transactions.push(transaction);
+    });
+
+    await Promise.all(promises).then(() => {});
 
     return transactions;
   }
